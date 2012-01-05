0772490da1d6bf04c2dcc8051d875e2080c8d9e4
Doug Carlson <carlson@cs.jhu.edu>
2012-01-05 08:38:52 -0500

    trying to fix alignment

--- apps/breakfast/util/i2cTLVStorage/I2CTLVStorage.h
+++ apps/breakfast/util/i2cTLVStorage/I2CTLVStorage.h
@@ -9,12 +9,11 @@
 #define TLV_STORAGE_READ_CMD  0xa1
 
 typedef union{
-  uint8_t buf[TLV_STORAGE_REGISTER_LEN];
+  uint8_t buf[TLV_LEN];
   struct{
-    uint8_t cmd;
-    uint8_t padding;
-    uint8_t buf[TLV_STORAGE_REGISTER_LEN - 2];
-  } s;
+    uint16_t cmd;
+    uint8_t buf[TLV_LEN];
+  } s __attribute__ ((aligned(16)));
 } __attribute__((__packed__)) tlv_register_t;
 
 
