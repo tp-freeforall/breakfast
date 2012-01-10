e7514e1b806e708de08a783c24f2080e6cc7056b
Doug Carlson <carlson@cs.jhu.edu>
2012-01-10 13:20:50 -0500

    testing toast in isolation

--- apps/breakfast/toast/testToastClock/c/burn.sh
+++ apps/breakfast/toast/testToastClock/c/burn.sh
@@ -9,9 +9,8 @@ fi
 comport=$1
 file=$2
 shift 2
-
-/opt/osian/tinyos/tools/platforms/msp430/msp430-bsl/msp430-bsl.py \
-  --invert-reset --invert-test -c $comport -r $@ $file
+set -x
+/home/carlson/local/src/tinyos-2.x/tools/platforms/msp430/msp430-bsl/msp430-bsl.py --invert-reset --invert-test -c $comport -r -e -I -p $file
 
 ##to read the calibration values out of memory:
 #msp430-bsl.py -c /dev/ttyUSB0 --upload=0x10F8 --size=8 --invert-test --invert-reset --password <your_file.o>
--- apps/breakfast/toast/testToastClock/c/testclocks.c
+++ apps/breakfast/toast/testToastClock/c/testclocks.c
@@ -1,4 +1,4 @@
-#include  <msp430x23x.h>
+#include  <msp430.h>
 
 //mclk: 20mhz
 //aclk: 32khz
@@ -42,7 +42,7 @@ void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
 
-  clock_1mhz();
+  clock_default();
 
   P6DIR |= 0x01;                            // Set P6.0 to output direction
   P5DIR |= 0x50;                            // Set p5.4, p5.6 output
