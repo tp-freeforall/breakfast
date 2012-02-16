97e1a0b53fb15ef1c2c31354f125a7c2173aa6a5
Doug Carlson <carlson@cs.jhu.edu>
2012-02-16 11:22:10 -0500

    another script for outdoor testing

---
+++ apps/breakfast/bacon/testRadio/scripts/rxOnly.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+if [ $# -lt 2 ]
+then
+    echo "Usage: $0 <duration> <label>" 1>&2
+    exit 1
+fi
+
+duration=$1
+prefix=$2
+numMotes=$(motelist | grep -c "/dev/ttyUSB")
+echo "Logging for $numMotes devices for $duration seconds"
+
+if [ -f logjobs ]
+then
+    echo "Killing old jobs"
+    for pid in $(cat logjobs)
+    do
+        kill $pid
+    done
+    rm logjobs
+fi
+
+date +%s > $prefix.startTime
+
+for dev in $(motelist | awk '/USB/{print $2}')
+do
+    python $(dirname $0)/rawSerialTS.py $dev > $prefix.$(basename $dev).rx &
+    echo "$!" >> logjobs
+done
+stopTime=$(( $(date +%s) + $duration))
+while [ $(date +%s) -lt $stopTime ]
+do
+  echo "$(($stopTime - $(date +%s))) seconds left: "
+  wc -l $prefix.*
+  sleep 5
+  echo "-----"
+done
+
+for pid in $(cat logjobs)
+do
+    kill $pid
+done
+rm logjobs
